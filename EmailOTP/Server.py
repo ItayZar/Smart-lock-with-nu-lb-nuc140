271032fc76d2a17e0a29f3a63d21c8d8b4037669
ItayZar <82230021+ItayZar@users.noreply.github.com>
2023-06-28 18:22:47 +0300

    Update Server.py with comments

--- EmailOTP/Server.py
+++ EmailOTP/Server.py
@@ -2,39 +2,34 @@ import bluetooth
 import re
 import random
 from SendEmail import send_email_message,gmail_init
+from config.py import hc05_address
 
-# Bluetooth MAC address of the HC-05 module
-hc05_address = '20:17:01:03:41:02'  # Replace with the actual address
-
-# Create a Bluetooth socket
+# Create a Bluetooth socket and connect to the HC-05 module
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
-# Connect to the HC-05 module
 sock.connect((hc05_address, 1))  # Port number 1 for RFCOMM
 
-gmail_init()
+gmail_init() 
 
 def generate_otp():
+    """ Generates OTP of 4 digits between 1 to 9 because the keypad can not take 0 as an input """
     otp = ""
     for _ in range(4):
         digit = random.randint(1, 9)
         otp += str(digit)
     return otp
 
-# Receive and display messages
 while True:
-    data = sock.recv(1024)  # Adjust the buffer size as per your requirements
+    data = sock.recv(1024)  # Buffer adjustment 
     data = data.decode('utf-8', 'ignore')  # Convert bytes to string
     data = data.strip()  # Strip whitespace
-    data = re.sub(r'[^\x20-\x7E]', '', data)  # Filter non-printable characters
+    data = re.sub(r'[^\x20-\x7E]', '', data)  # Filter non-printable characters in case the data is sent with "\n" or "\r"
     if data:
-        #print("Received message:", data)  # Display the received message
-        myotp=generate_otp()
-        send_email_message(otp=myotp,id=data)  # Send the message via email
+        myotp=generate_otp() # Generate OTP for authentication
+        send_email_message(otp=myotp,id=data)  # Send the OTP via email to the specific user
         try:
             sock.send(myotp.encode('utf-8'))  # Send the OTP to the Bluetooth device
         except Exception as e:
-            print("Error sending data:", str(e))
+            print("Error sending data:", str(e)) # In case of error
 
 # Close the socket connection when done
 sock.close()
