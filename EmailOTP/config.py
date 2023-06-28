423dcb8bfa2b72e997097bf629bd9380db566d53
ItayZar <82230021+ItayZar@users.noreply.github.com>
2023-06-28 21:36:52 +0300

    Update config.py

--- EmailOTP/config.py
+++ EmailOTP/config.py
@@ -1,6 +1,6 @@
 # Users email addresses dictionary 
 # The id should be matched to the ones in the Smart-lock-with-nu-lb-nuc140.c 
-users={'0':'itayzar10@gmail.com','1':'elimakora@gmail.com'}
+users={'0':'e1@gmail.com','1':'e2@gmail.com'}
 
 # Bluetooth MAC address of the HC-05 module
 hc05_address = '20:17:01:03:41:02' 
